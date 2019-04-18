44eafd01f1d69fa13c0c1f7074af439f90788c7f
U-ADS\dpltech <dpltech@bl-libg-ltdplab.ads.iu.edu>
2019-04-18 09:50:00 -0400

    updated launcher for python 3

---
+++ BDPL_launch_PY3.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+
+:_START
+CLS
+TYPE C:\BDPL\scripts\bdpl.txt
+ECHO.
+ECHO.
+ECHO.
+
+setlocal EnableDelayedExpansion
+IF NOT EXIST Z: (
+  REM Get username
+  SET /P _name="Enter your IU username: "
+ 
+  REM get server
+  REM SET /P _server=<C:\BDPL\resources\server.txt
+
+  REM Connect to shared drive
+  NET USE Z: \\156.56.241.173\bdpl /user:ads\!_name! *
+)
+
+IF NOT EXIST Z: (
+  PAUSE
+  GOTO _START
+) ELSE (
+  CLS
+)
+
+
+TYPE C:\BDPL\scripts\bdpl.txt
+"C:\Python27\python.exe" C:\BDPL\scripts\python3\bdpl_ingest_20190329.pyw
+
+
+
+
