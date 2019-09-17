8b7ce3ace65bbcaf38f8af3086110a4633d2df39
U-ADS\micshall <micshall@bl-libg-ltdplib.ads.iu.edu>
2019-09-17 10:15:42 -0400

    Standalone script to fix dates using DFXML

---
+++ fix_dates_manual.py
@@ -0,0 +1,113 @@
+from collections import OrderedDict
+from collections import Counter
+import csv
+import datetime
+import errno
+import math
+import os
+import re
+import shutil
+import sqlite3
+import subprocess
+import sys
+import uuid
+import xml
+import lxml
+from lxml import etree
+import tempfile
+import fnmatch
+from tkinter import *
+import tkinter.filedialog
+from tkinter import ttk
+import glob
+import pickle
+import time
+import openpyxl
+import glob
+import hashlib
+
+def premis_dict(timestamp, event_type, event_outcome, event_detail, event_detail_note, agent_id):
+    temp_dict = {}
+    temp_dict['eventType'] = event_type
+    temp_dict['eventOutcomeDetail'] = event_outcome
+    temp_dict['timestamp'] = timestamp
+    temp_dict['eventDetailInfo'] = event_detail
+    temp_dict['eventDetailInfo_additional'] = event_detail_note
+    temp_dict['linkingAgentIDvalue'] = agent_id
+    return temp_dict
+
+def main():
+    print('\n\nFILE MAC TIME CORRECTION (USING DFXML)')
+
+    target = input('Barcode directory: ')
+    dfxml_output = input('DFXML file: ')
+
+    files_dir = os.path.join(target, 'files')
+
+    premis_file = os.path.join(target, 'temp', 'premis_list.txt')
+
+
+    timestamp = str(datetime.datetime.now())
+     
+    try:
+        for (event, obj) in Objects.iterparse(dfxml_output):
+            # only work on FileObjects
+            if not isinstance(obj, Objects.FileObject):
+                continue
+
+            # skip directories and links
+            if obj.name_type:
+                if obj.name_type not in ["r", "d"]:
+                    continue
+
+            # record filename
+            dfxml_filename = obj.filename
+            dfxml_filedate = int(time.time()) # default to current time
+
+            # record last modified or last created date
+            try:
+                mtime = obj.mtime
+                mtime = str(mtime)
+            except:
+                pass
+
+            try:
+                crtime = obj.crtime
+                crtime = str(crtime)
+            except:
+                pass
+
+            # fallback to created date if last modified doesn't exist
+            if mtime and (mtime != 'None'):
+                mtime = time_to_int(mtime[:19])
+                dfxml_filedate = mtime
+            elif crtime and (crtime != 'None'):
+                crtime = time_to_int(crtime[:19])
+                dfxml_filedate = crtime
+            else:
+                continue
+
+            # rewrite last modified date of corresponding file in objects/files
+            exported_filepath = os.path.join(files_dir, dfxml_filename)
+            if os.path.isdir(exported_filepath):
+                os.utime(exported_filepath, (dfxml_filedate, dfxml_filedate))
+            elif os.path.isfile(exported_filepath):
+                os.utime(exported_filepath, (dfxml_filedate, dfxml_filedate)) 
+            else:
+                continue
+
+    except (ValueError, xml.etree.ElementTree.ParseError):
+        print('\nUnable to read DFXML!')
+        pass
+        
+    with open(premis_file, 'rb') as f:
+        premis_list = pickle.load(f)
+        
+    premis_list.append(premis_dict(timestamp, 'metadata modification', 0, 'https://github.com/CCA-Public/diskimageprocessor/blob/master/diskimageprocessor.py#L446-L489', 'Corrected file timestamps to match information extracted from disk image.', 'Adapted from Disk Image Processor Version: 1.0.0 (Tim Walsh)'))
+    
+    with open(premis_file, 'wb') as f:
+        pickle.dump(premis_list, f)
+    pickleDump('premis_list', premis_list)
+        
+if __name__ == "__main__":
+    main()
