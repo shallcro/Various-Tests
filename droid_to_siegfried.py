7ae4ce9afbe7e0f628398367e4733abaad40025f
U-ADS\micshall <micshall@bl-libg-ltdplib.ads.iu.edu>
2019-09-25 10:10:40 -0400

    Improve parsing of DROID output to migrate to Siegfried output

--- droid_to_siegfried.py
+++ droid_to_siegfried.py
@@ -5,6 +5,8 @@ infile = input('Enter path to csv: ')
 
 report_dir = os.path.dirname(infile)
 
+unidentified = os.path.join(report_dir, 'unidentified.csv')
+
 outfile = os.path.join(report_dir, 'siegfried.csv')
 counter = 0
 with open(outfile, 'w', newline='') as f1:
@@ -17,9 +19,30 @@ with open(outfile, 'w', newline='') as f1:
         for row in csvReader:
             counter+=1
             print('\rWorking on row %d' % counter, end='')
-            if row[11] == 'TRUE':
+            
+            filename = row[2].split('file:/', 1)[1]
+            filesize = row[7]
+            modified = row[10]
+            errors = ''
+            namespace = 'pronom'
+            if row[14] == "":
+                id = 'UNKNOWN'
+            else:
+                id = row[14]
+            format = row[16]
+            version = row[17]
+            mime = row[15]
+            basis = ''
+            if row[11].lower() == 'true':
                 warning = 'extension mismatch'
             else:
                 warning = ''
-            data = [row[2].split('file:/', 1)[1], row[7], row[10], '', 'pronom', row[14], row[16], row[15], '', '']
+            
+            
+            data = [filename, filesize, modified, errors, namespace, id, format, version, mime, basis, warning]
             csvWriter.writerow(data)
+            
+            if id == 'UNKNOWN':
+                with open(unidentified, 'a', newline='') as f3:
+                    csvWriter2 = csv.writer(f3)
+                    csvWriter2.writerow(data)
